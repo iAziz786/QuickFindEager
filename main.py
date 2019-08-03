bb3376ffb82aede5839e3ab9de08930ec70a621b
Mohammad Aziz <mdaziz067@gmail.com>
2019-08-03 11:18:03 +0530

    Add Quick Find

---
+++ main.py
@@ -0,0 +1,28 @@
+class QuickFind():
+  """
+  We provide some list and QuickFind give us the answer
+  whether they are connected or not.
+  #### Example
+  ```
+    qks = QuickFind()
+    qks.join(2, 4)
+    qks.join(4, 8)
+    qks.is_connected(2, 8) // True
+  ```
+  """
+  def __init__(self, size):
+    """
+    @params size: Max size of the array
+    """
+    self.quick_list = [i for i in range(size)]
+
+  def join(self, node1, node2):
+    value = self.quick_list[node2]
+    for i in range(len(self.quick_list)):
+      if (self.quick_list[i] == value):
+        self.quick_list[i] = self.quick_list[node1]
+    print(self.quick_list)
+  
+  def is_connected(self, first, second):
+    """is_connected(first, second) -> bool"""
+    return (self.quick_list[first] == self.quick_list[second])
---
+++ test.py
@@ -0,0 +1,17 @@
+import unittest
+from .main import QuickFind
+
+class TestQuickFindEager(unittest.TestCase):
+
+    def test_upper(self):
+      quick = QuickFind(10)
+      quick.join(5, 6)
+      quick.join(6, 7)
+      quick.join(1, 5)
+      quick.join(0, 1)
+      
+      self.assertEqual(quick.is_connected(1, 7), False)
+
+
+if __name__ == '__main__':
+    unittest.main()
